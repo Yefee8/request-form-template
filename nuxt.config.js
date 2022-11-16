6c95c96dfb468f075a083d10eec60fed26121543
Yavuz Efe <75783774+Yefee8@users.noreply.github.com>
2022-11-16 12:57:23 +0300

    font bug fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,10 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
+    link:[{rel:'preconnect', href: 'https://fonts.googleapis.com'}, 
+          {rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+          {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap'}
+    ],
     meta: [
       {
         charset:
